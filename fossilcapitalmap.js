d048f196cb6daa42dd39c9ac6d965631a71cab77
aurashak <154039021+aurashak@users.noreply.github.com>
2024-02-11 13:15:25 -0500

    Add files via upload

--- fossilcapitalmap.js
+++ fossilcapitalmap.js
@@ -2,18 +2,18 @@ var map = L.map('map').setView([0, 0], 2); // Centered at California and zoom le
 
 // Purple icon
 var purpleIcon = L.icon({
-  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-purple.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  tooltipAnchor: [16, -28],
-  shadowSize: [41, 41]
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-purple.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    tooltipAnchor: [16, -28],
+    shadowSize: [41, 41]
 });
 
 // GeoJSON layer for purple markers with clustering
 var markers = L.markerClusterGroup({
-  maxClusterRadius: 80, // Adjust this value based on your preference
-  spiderfyDistanceMultiplier: 2 // Adjust this value based on your preference
+    maxClusterRadius: 80, // Adjust this value based on your preference
+    spiderfyDistanceMultiplier: 2 // Adjust this value based on your preference
 });
 
 // GeoJSON layer for red lines
@@ -21,51 +21,51 @@ var redLines = L.layerGroup(); // Initialize as an empty layerGroup
 
 // Fetch GeoJSON data for purple markers
 fetch('https://aurashak.github.io/fossilcapital/gisfiles/californiamines.geojson')
-  .then(response => response.json())
-  .then(data => {
-    L.geoJSON(data, {
-      pointToLayer: function (feature, latlng) {
-        return L.marker(latlng, { icon: purpleIcon });
-      },
-      onEachFeature: function (feature, layer) {
-        layer.bindPopup(feature.properties.name); // You can customize the popup content
-      }
-    }).addTo(markers);
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data, {
+            pointToLayer: function (feature, latlng) {
+                return L.marker(latlng, { icon: purpleIcon });
+            },
+            onEachFeature: function (feature, layer) {
+                layer.bindPopup(feature.properties.name); // You can customize the popup content
+            }
+        }).addTo(markers);
 
-    map.addLayer(markers);
-  })
-  .catch(error => console.error('Error fetching GeoJSON for purple markers:', error));
+        map.addLayer(markers);
+    })
+    .catch(error => console.error('Error fetching GeoJSON for purple markers:', error));
 
 // Fetch GeoJSON data for red lines
 fetch('https://aurashak.github.io/fossilcapital/gisfiles/naturalgaspipelines.geojson')
-  .then(response => response.json())
-  .then(data => {
-    L.geoJSON(data, {
-      style: function (feature) {
-        return {
-          color: 'red',
-          weight: 2
-        };
-      }
-    }).addTo(redLines);
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data, {
+            style: function (feature) {
+                return {
+                    color: 'red',
+                    weight: 2
+                };
+            }
+        }).addTo(redLines);
 
-    // Add redLines to map after it's defined
-    map.addLayer(redLines);
-  })
-  .catch(error => console.error('Error fetching GeoJSON for red lines:', error));
+        // Add redLines to map after it's defined
+        map.addLayer(redLines);
+    })
+    .catch(error => console.error('Error fetching GeoJSON for red lines:', error));
 
 // Satellite layer
 var satelliteLayer = L.tileLayer.provider('Esri.WorldImagery');
 
 // OSM layer
 var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '© OpenStreetMap contributors'
+    attribution: '© OpenStreetMap contributors'
 });
 
 // Leaflet layers control
 var baseLayers = {
-  'Satellite': satelliteLayer,
-  'OpenStreetMap': osmLayer
+    'Satellite': satelliteLayer,
+    'OpenStreetMap': osmLayer
 };
 
 // Set maxZoom for the map
@@ -73,8 +73,8 @@ map.setMaxZoom(18); // Adjust this value based on your preference
 
 // Overlay layers control
 var overlayLayers = {
-  'Purple Markers': markers,
-  'Red Lines': redLines
+    'Purple Markers': markers,
+    'Red Lines': redLines
 };
 
 L.control.layers(baseLayers, overlayLayers).addTo(map);
@@ -82,3 +82,18 @@ L.control.layers(baseLayers, overlayLayers).addTo(map);
 // Set default layers
 satelliteLayer.addTo(map);
 markers.addTo(map); // Add purple markers by default
+
+// Legend
+var legend = L.control({ position: 'bottomright' });
+
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend');
+    div.innerHTML +=
+        '<img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-purple.png">' +
+        ' Purple Markers<br>' +
+        '<div style="background-color: red; width: 25px; height: 5px; display: inline-block;"></div>' +
+        ' Red Lines';
+    return div;
+};
+
+legend.addTo(map);
