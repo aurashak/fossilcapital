ab19cb78ab1f12385871e11ed10199cbabe33dfc
aurashak <154039021+aurashak@users.noreply.github.com>
2024-02-11 12:34:35 -0500

    Add files via upload

--- fossilcapitalmap.js
+++ fossilcapitalmap.js
@@ -1,4 +1,21 @@
-var map = L.map('map').setView([0, 0], 2); // Centered at [0, 0] and zoom level 2
+var map = L.map('map').setView([37.7749, -122.4194], 6); // Centered at California and zoom level 6
+
+// Purple icon
+var purpleIcon = L.icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-purple.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+
+// GeoJSON layer with purple markers
+L.geoJSON.ajax('https://aurashak.github.io/fossilcapital/gisfiles/californiamines.geojson', {
+  pointToLayer: function (feature, latlng) {
+    return L.marker(latlng, { icon: purpleIcon });
+  }
+}).addTo(map);
 
 // Satellite layer
 var satelliteLayer = L.tileLayer.provider('Esri.WorldImagery');
