bbfa8511294fed997f21886b0eae5fbf25e5e228
aurashak <154039021+aurashak@users.noreply.github.com>
2024-02-11 19:26:47 -0500

    Add files via upload

--- fossilcapitalmap.js
+++ fossilcapitalmap.js
@@ -87,10 +87,10 @@ legend.onAdd = function (map) {
     var div = L.DomUtil.create('div', 'info legend');
     div.innerHTML =
         '<div style="background-color: yellow; width: 25px; height: 25px; border-radius: 50%; display: inline-block; vertical-align: middle;"></div>' +
-        '<span style="display: inline-block; vertical-align: middle; line-height: 25px;"> California Mines</span>' +
+        '<span style="display: inline-block; vertical-align: middle; line-height: 25px;"> &nbsp; California Mines</span>' +
         '<br>' +
-        '<div style="background-color: red; width: 25px; height: 5px; display: inline-block; vertical-align: middle;"></div>' +
-        '<span style="display: inline-block; vertical-align: middle; line-height: 5px;"> Natural Gas Pipelines</span>';
+        '<div style="background-color: red; width: 25px; height: 5px; display: inline-block; margin-left:"></div>' +
+        '<span style="display: inline-block; vertical-align: middle; line-height: 5px;"> &nbsp;  Natural Gas Pipelines</span>';
     return div;
 };
 
